9d17b8de0d3279d8a7defc765d19a6522bff1a84
moonkkjs <giovanaaguiaroficial@gmail.com>
2024-11-20 23:18:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'vida animal',
+    'qual o maior ornitorrinco do mundo?',
+    'Ornintorrinco-zilla'
+)
+
+criaCartao(
+    'vida animal',
+    'existem quantas raças de cachorro?',
+    'existem 344 raças'
+)
+
+criaCartao(
+    'vida animal',
+    'Quantas raças de gatos existem?',
+    'Existem 71 raças de gato'
+)
+
+criaCartao(
+    'vida animal',
+    'Qual o gato mais raro do mundo?',
+    'é o gato lykoi'
+)
